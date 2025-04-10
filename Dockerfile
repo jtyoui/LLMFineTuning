2fedcfa1278e17cdee3c2d0b329081d606ae6e2b
zhangwei <jtyoui@qq.com>
2025-04-10 16:55:57 +0800

    修改测试不能加载模型

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,9 @@ FROM pytorch/pytorch:2.6.0-cuda12.4-cudnn9-runtime
 
 WORKDIR /app
 
-ENV LLM /llm
-
 COPY . .
+
+RUN apt update && apt install -y build-essential
 
 RUN pip3 install -r requirements.txt
 RUN pip3 install -U xformers --index-url https://download.pytorch.org/whl/cu124
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from os import path
 
 import pandas as pd
 
-llm = os.environ["LLM"]
+llm = os.environ.get("LLM", "/llm")
 
 with open(path.join(llm, "config.toml"), "rb") as f:
     config = tomllib.load(f)
@@ -14,15 +14,23 @@ if "Envs" in config:
     for k, v in config['Envs'].items():
         os.environ[k] = str(v)
 
+# 重写路径
 if not config["Pretrained"]["model_name"].startswith("/"):
     config["Pretrained"]["model_name"] = path.join(llm, config["Pretrained"]["model_name"])
 
-pprint.pprint(config)
+if not config['SFTConfig']['output_dir'].startswith("/"):
+    config["SFTConfig"]["output_dir"] = path.join(llm, config["SFTConfig"]["output_dir"])
+
+if not config['args']['data_path'].startswith("/"):
+    config['args']['data_path'] = path.join(llm, config['args']['data_path'])
+
+# 如果策略是测试，那么加载的模型来自于output_dir
+if config['args']['strategy'] == 'test':
+    config['Pretrained']['model_name'] = config['SFTConfig']['output_dir']
+
+pprint.pprint(config, indent=2)
 
 data = config['args']['data_path']
-if not data.startswith("/"):
-    data = path.join(llm, data)
-
 suffix = os.path.splitext(data)[-1]
 
 if suffix == '.xlsx':
